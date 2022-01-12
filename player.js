99a9fcf6067a3408da298f56e618bd7d7fceae5d
ugurctaygun <ugurctaygun@gmail.com>
2022-01-12 15:47:19 +0300

    draw player

---
+++ player.js
@@ -0,0 +1,28 @@
+export default class Player {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.states = [];
+    this.currentState = this.states[0];
+    this.image = document.getElementById("theDog");
+    this.width = 200;
+    this.height = 181.83;
+    this.x = 0;
+    this.y = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+  }
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.width * this.frameX,
+      this.width * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
